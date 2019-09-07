2a74adbaf3cbbfea90e644fccad4ed998012370b
mccarrion <mccarrion@users.noreply.github.com>
2019-09-06 23:47:51 -0700

    Made all 7 tetris shapes

--- cpp_testground/cpp_testground.cpp
+++ cpp_testground/cpp_testground.cpp
@@ -15,39 +15,34 @@ int main()
 	tetromino[0].append(L"..X.");
 
 	tetromino[1].append(L"..X.");
-	tetromino[1].append(L"..X.");
-	tetromino[1].append(L"..X.");
-	tetromino[1].append(L"..X.");
+	tetromino[1].append(L".XX.");
+	tetromino[1].append(L".X..");
+	tetromino[1].append(L"....");
 
+	tetromino[2].append(L".X..");
+	tetromino[2].append(L".XX.");
 	tetromino[2].append(L"..X.");
-	tetromino[2].append(L"..X.");
-	tetromino[2].append(L"..X.");
-	tetromino[2].append(L"..X.");
+	tetromino[2].append(L"....");
 
-	tetromino[3].append(L"..X.");
-	tetromino[3].append(L"..X.");
-	tetromino[3].append(L"..X.");
-	tetromino[3].append(L"..X.");
+	tetromino[3].append(L"....");
+	tetromino[3].append(L".XX.");
+	tetromino[3].append(L".XX.");
+	tetromino[3].append(L"....");
 
 	tetromino[4].append(L"..X.");
+	tetromino[4].append(L".XX.");
 	tetromino[4].append(L"..X.");
-	tetromino[4].append(L"..X.");
-	tetromino[4].append(L"..X.");
+	tetromino[4].append(L"....");
 
-	tetromino[5].append(L"..X.");
-	tetromino[5].append(L"..X.");
+	tetromino[5].append(L"....");
+	tetromino[5].append(L".XX.");
 	tetromino[5].append(L"..X.");
 	tetromino[5].append(L"..X.");
 	
-	tetromino[6].append(L"..X.");
-	tetromino[6].append(L"..X.");
-	tetromino[6].append(L"..X.");
-	tetromino[6].append(L"..X.");
-
-	tetromino[7].append(L"..X.");
-	tetromino[7].append(L"..X.");
-	tetromino[7].append(L"..X.");
-	tetromino[7].append(L"..X.");
+	tetromino[6].append(L"....");
+	tetromino[6].append(L".XX.");
+	tetromino[6].append(L".X..");
+	tetromino[6].append(L".X..");
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
