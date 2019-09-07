dc1d20635a79fc663255bf55d25d2297d5aee4fb
mccarrion <mccarrion@users.noreply.github.com>
2019-09-07 14:52:40 -0700

    Collision detection and drawing game field

--- cpp_testground/cpp_testground.cpp
+++ cpp_testground/cpp_testground.cpp
@@ -2,12 +2,16 @@
 //
 
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 wstring tetromino[7];
 int nFieldWidth = 12;
 int nFieldHeight = 18;
 unsigned char* pField = nullptr;
+
+int nScreenWidth = 80;		// Console Screen Size X (columns)
+int nScreenHeight = 30;		// Console Screen Size Y (rows)
 
 /**
  * The tetris pieces exist in a 4X4 matrix
@@ -22,6 +26,34 @@ int Rotate(int px, int py, int r)
 	case 1: return 12 + py - (px * 4);	// 90 degrees
 	case 2: return 15 - (py * 4) - px;	// 180 degrees
 	}
+}
+
+bool DoesPieceFit(int nTetromino, int nRotation, int nPosX, int nPosY)
+{
+	for (int px = 0; px < 4; px++)
+	{
+		for (int py = 0; py < 4; py++)
+		{
+			// Get index into piece
+			int pi = Rotate(px, py, nRotation);
+
+			// Get index into field
+			int fi = (nPosY + py) * nFieldWidth + (nPosX + px);
+
+			if (nPosX + px >= 0 && nPosX + px < nFieldWidth)
+			{
+				if (nPosY + py >= 0 && nPosY + py < nFieldHeight)
+				{
+					if (tetromino[nTetromino][pi] == L'X' && pField[fi] != 0)
+					{
+						return false; // fail on first hit
+					}
+				}
+			}
+		}
+	}
+
+	return true;
 }
 
 int main()
@@ -69,6 +101,38 @@ int main()
 		{
 			pField[y * nFieldWidth + x] = (x == 0 || x == nFieldWidth - 1 || y == nFieldHeight - 1) ? 9 : 0;
 		}
+	}
+
+	// For drawing on screen
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	for (int i = 0; i < nScreenWidth * nScreenHeight; i++) screen[i] = L' ';
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+	bool bGameOver = false;
+
+	while (!bGameOver) 
+	{
+		// GAME TIMING ======================================
+
+		// INPUT ============================================
+
+		// GAME LOGIC =======================================
+
+		// RENDER OUTPUT ====================================
+
+		// Draw Field
+		for (int x = 0; x < nFieldWidth; x++)
+		{
+			for (int y = 0; y < nFieldHeight; y++)
+			{
+				screen[(y + 2) * nScreenWidth + (x + 2)] = L" ABCDEFG=#"[pField[y * nFieldWidth + x]];
+			}
+		}
+
+		// Display frame
+		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
 	}
 
 	return 0;
