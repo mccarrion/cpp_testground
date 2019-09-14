0f51a5768c9ec756552dd2fbc9bea93d9a96f712
mccarrion <mccarrion@users.noreply.github.com>
2019-09-14 15:10:26 -0700

    Initialized first person ascii maze

---
+++ first_person/FirstPerson/FirstPerson/FirstPerson.cpp
@@ -0,0 +1,23 @@
+// This is a first person maze explorer based on an online tutorial
+
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
+int main()
+{
+	// Create Screen Buffer
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+	screen[nScreenWidth * nScreenHeight - 1] = '\0';
+	WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+
+	return 0;
+}
