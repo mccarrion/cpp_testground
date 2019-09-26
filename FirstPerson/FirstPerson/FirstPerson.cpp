c2cc111f8510cb31642d0690645cf74fdb25ede5
mccarrion <mccarrion@users.noreply.github.com>
2019-09-25 19:59:13 -0700

    Finished up maze game

--- FirstPerson/FirstPerson/FirstPerson.cpp
+++ FirstPerson/FirstPerson/FirstPerson.cpp
@@ -149,7 +149,7 @@ int main()
 						float fBound = 0.01;
 						if (acos(p.at(0).second) < fBound) bBoundary = true;
 						if (acos(p.at(1).second) < fBound) bBoundary = true;
-						if (acos(p.at(2).second) < fBound) bBoundary = true;
+						//if (acos(p.at(2).second) < fBound) bBoundary = true;
 
 					}
 				}
@@ -191,6 +191,20 @@ int main()
 				}
 			}
 		}
+
+		// Display Stats
+		swprintf_s(screen, 40, L"X=%3.2f, Y=%3.2f, A=%3.2f FPS=%3.2f ", fPlayerX, fPlayerY, fPlayerA, 1.0f / fElapsedTime);
+
+		// Display Map
+		for (int nx = 0; nx < nMaptWidth; nx++)
+		{
+			for (int ny = 0; ny < nMapWidth; ny++)
+			{
+				screen[(ny + 1) * nScreenWidth + nx] = map[ny * nMapWidth + nx];
+			}
+		}
+
+		screen[((int)fPlayerY + 1) * nScreenWidth + (int)fPlayerX] = 'P';
 
 		screen[nScreenWidth * nScreenHeight - 1] = '\0';
 		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
