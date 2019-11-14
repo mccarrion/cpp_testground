6a3e9a093041d67891c211d214a95972f11f5e1f
mccarrion <mccarrion@users.noreply.github.com>
2019-11-13 21:37:55 -0700

    Drawing window now

--- pong/src/main.rs
+++ pong/src/main.rs
@@ -18,7 +18,21 @@ fn main() -> amethyst::Result<()> {
     let app_root = application_root_dir()?;
     let display_config_path = app_root.join("config").join("display.ron");
     
-    let game_data = GameDataBuilder::default();
+    let mut world = World::new();
+
+    /**
+     * RenderToWindow provides scaffolding to open a window and draw in it
+     * RenderFlat2D is used to render entities with `SpriteRender` component
+     */
+    let game_data = GameDataBuilder::default()
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+            .with_plugin(
+                RenderToWindow::from_config_path(display_config_path)
+                    .with_clear([0.0, 0.0, 0.0, 1.0]),
+            )
+            .with_plugin(RenderFlat2D::default()),
+        )?;
     
     let assets_dir = app_root.join("assets");
     let mut world = World::new();
