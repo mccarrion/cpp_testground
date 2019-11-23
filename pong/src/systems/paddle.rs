aaf28b5776bd20a89fd9303bfa7e1b4bc857caa4
mccarrion <mccarrion@users.noreply.github.com>
2019-11-23 11:29:29 -0700

    Added paddle system

---
+++ pong/src/systems/paddle.rs
@@ -0,0 +1,44 @@
+use amethyst::core::{Transform, SystemDesc};
+use amethyst::derive::SystemDesc;
+use amethyst::ecs::{
+    Join, 
+    Read, 
+    ReadStorage, 
+    System, 
+    SystemData, 
+    World, 
+    WriteStorage
+};
+use amethyst::input::{InputHandler, StringBindings};
+
+// You'll have to mark PADDLE_HEIGHT as public in pong.rs
+use crate::pong::{Paddle, Side, ARENA_HEIGHT, PADDLE_HEIGHT};
+
+#[derive(SystemDesc)]
+pub struct PaddleSystem;
+
+impl<'s> System<'s> for PaddleSystem {
+    type SystemData = (
+        WriteStorage<'s, Transform>,
+        ReadStorage<'s, Paddle>,
+        Read<'s, InputHandler<StringBindings>>,
+    );
+
+    fn run(&mut self, (mut transforms, paddles, input): Self::SystemData) {
+        for (paddle, transform) in (&paddles, &mut transforms).join() {
+            let movement = match paddle.side {
+                Side::Left => input.axis_value("left_paddle"),
+                Side::Right => input.axis_value("right_paddle"),
+            };
+            if let Some(mv_amount) = movement {
+                if mv_amount != 0.0 {
+                    let side_name = match paddle.side {
+                        Side::Left => "left",
+                        Side::Right => "right",
+                    };
+                    println!("Side {:?} moving {}", side_name, mv_amount)
+                }
+            }
+        }
+    }
+}
